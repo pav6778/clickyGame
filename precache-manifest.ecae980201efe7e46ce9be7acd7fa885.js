ef964e9cf056408c25b1f5e373bf7de757b34d4e
pav6778 <panchukovp@gmail.com>
2020-01-05 16:21:38 -0500

    Updates

---
+++ precache-manifest.ecae980201efe7e46ce9be7acd7fa885.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed41d01dbda6b9dc7cda201cf63386a",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "e25754fe33e56b4b7ad8",
+    "url": "/clickyGame/static/css/main.8f114b66.chunk.css"
+  },
+  {
+    "revision": "da2148e6f4bb4e003925",
+    "url": "/clickyGame/static/js/2.4113a606.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickyGame/static/js/2.4113a606.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e25754fe33e56b4b7ad8",
+    "url": "/clickyGame/static/js/main.cb8c1626.chunk.js"
+  },
+  {
+    "revision": "8da43c4928d082bedbb4",
+    "url": "/clickyGame/static/js/runtime-main.962eef43.js"
+  }
+]);
